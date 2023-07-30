67298f3b3de7257c81896a408717348b4b981ef5
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2023-07-30 16:10:48 +0200

    Added a Key-Value storage via UserDefaults

---
+++ swifty_companion/Services/KeyValueStorage.swift
@@ -0,0 +1,9 @@
+//
+//  KeyValueStorage.swift
+//  swifty_companion
+//
+//  Created by Zuleykha Pavlichenkova on 30.07.2023.
+//  Copyright © 2023 Heidi Merianne. All rights reserved.
+//
+
+import Foundation
---
+++ swifty_companionTests/Services/KeyValueStorageTests.swift
@@ -0,0 +1,9 @@
+//
+//  KeyValueStorageTests.swift
+//  swifty_companionTests
+//
+//  Created by Zuleykha Pavlichenkova on 30.07.2023.
+//  Copyright © 2023 Heidi Merianne. All rights reserved.
+//
+
+import Foundation
